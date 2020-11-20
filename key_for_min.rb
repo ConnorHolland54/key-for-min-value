a5738ca99ee418ff4091642ac6d9f514ac4e2ba5
Connor Holland <supersoccergoalie123@gmail.com>
2020-11-20 13:08:34 -0700

    commit tree 4cb0692b38216a8698fe7816ea0c46cf9442dff2

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,25 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+    result = {}
+    current_key = ""
+    if name_hash.length == 0
+        return nil
+    else
+       name_hash.collect do |key, value|
+            if result.length == 0
+                result[key] = value
+                current_key = key
+            else
+                if value < result[current_key]
+                    result[key] = value
+                    current_key = key
+                end
+            end
+        end
+
+
+    end
+    return current_key
 
 end
